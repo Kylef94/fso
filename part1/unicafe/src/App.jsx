0009d6454adb65fb11b1b291eb958090a0e5f7f6
kyle farrugia <k.farrugia94@gmail.com>
2025-07-12 16:14:29 +0100

    exs 1.10 - 1.11 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,10 +10,16 @@ const FeedbackInput = ({goodClick, neutralClick, badClick}) => {
   </div>)
 }
 
+const StatisticsLine = ({text, value}) => {
+  return (<tr>
+            <td>{text}</td>
+            <td>{value}</td>
+          </tr>)
+}
 const Statistics = ({goodStat, neutralStat, badStat}) => {
   let total = goodStat + neutralStat + badStat;
   let average = (( goodStat + (-1 * badStat)) / total);
-  let positive = goodStat / total *100 ;
+  let positive = `${(goodStat / total * 100).toPrecision(4)} %` ;
 
   if (total === 0) {
     return <div className="statistics">No Feedback given</div>
@@ -21,11 +27,15 @@ const Statistics = ({goodStat, neutralStat, badStat}) => {
   return (
     <div className="statistics">
       <h1>Statistics</h1>
-      <p>good {goodStat}</p>
-      <p>neutral {neutralStat}</p>
-      <p>bad {badStat}</p>
-      <p>average {average}</p>
-      <p>positive {positive}%</p>
+      <table>
+        <tbody>
+          <StatisticsLine text="good" value={goodStat}/>
+          <StatisticsLine text="neutral" value={neutralStat}/>
+          <StatisticsLine text="bad" value={badStat}/>
+          <StatisticsLine text="average" value={average}/>
+          <StatisticsLine text="positive" value={positive}/>
+        </tbody>
+      </table>
     </div>
   )
 }
