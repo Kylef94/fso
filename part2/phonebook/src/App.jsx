f39ee7c5f0c44aaaf2b14fd9ae12cecc2d531aef
kyle farrugia <k.farrugia94@gmail.com>
2025-07-17 00:01:41 +0100

    ex 2.9 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: "07738128137"
-     }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
@@ -17,13 +19,30 @@ const App = () => {
     setNewNumber(e.target.value)
   }
 
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const filterByName = () => {
+    if (search === '') {
+      return persons
+    }
+    else {
+      return persons.filter((person) => person.name.toLowerCase().includes(search.toLowerCase()))
+    }
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault()
     if (persons.find(person => person.name == newName)) {
       alert(`${newName} is already in the phonebook!`)
     }
     else {
-      setPersons(persons.concat({name: newName, number: newNumber}))
+      setPersons(persons.concat({
+        name: newName, 
+        number: newNumber,
+        id: persons.length + 1
+      }))
     }
     setNewName('')
     setNewNumber('')
@@ -32,13 +51,16 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <form action="">
+        <div>filter shown with <input value={search} onChange={handleSearchChange} /></div>
+      </form>
       <form onSubmit={handleSubmit}>
         <div>name: <input value={newName} onChange={handleNameChange} /></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
         <div><button type="submit" >add</button></div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <p key={person.name}>{person.name} {person.number}</p>)}
+      {filterByName().map((person) => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
