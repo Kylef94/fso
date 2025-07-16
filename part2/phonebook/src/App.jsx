949caf34784a9964a13eba4cac6e085bc7dd1776
kyle farrugia <k.farrugia94@gmail.com>
2025-07-16 23:33:41 +0100

    ex 2.7 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,14 +6,17 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
   const handleInputChange = (e) => {
-    console.log(e.target.value)
     setNewName(e.target.value)
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log("new person added:", newName)
-    setPersons(persons.concat({name: newName}))
+    if (persons.find(person => person.name == newName)) {
+      alert(`${newName} is already in the phonebook!`)
+    }
+    else {
+      setPersons(persons.concat({name: newName}))
+    }
     setNewName('')
   }
 
